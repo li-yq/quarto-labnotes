3f3144ef31a59befa611b734314f6e66c675b30d
L-YQ <39945542+L-YQ@users.noreply.github.com>
2023-02-08 19:31:27 +0800

    ♻️ refactor git.lua for multiple repo

--- _extensions/labnotes/git.lua
+++ _extensions/labnotes/git.lua
@@ -1,7 +1,12 @@
 local out_dir = pandoc.path.join({ quarto.project.output_directory, "vcs" })
 
-local function normailize_rev(rev)
-    return pandoc.pipe("git", { "rev-parse", "--verify", rev }, ""):sub(1, 40)
+local function get_repo(meta, name)
+    local path = ( meta.repos or {} )[name] or "."
+    return pandoc.path.join({quarto.project.directory, path})
+end
+
+local function normailize_rev(repo, rev)
+    return pandoc.pipe("git", { "-C", repo, "rev-parse", "--verify", rev }, ""):sub(1, 40)
 end
 
 local function file_exists(name)
@@ -13,8 +18,8 @@ local function file_exists(name)
     return false
 end
 
-function retrive_file(rev, file)
-    rev = normailize_rev(rev)
+function retrive_file(repo, rev, file)
+    rev = normailize_rev(repo, rev)
     file = pandoc.path.normalize(file)
     local hash = pandoc.utils.sha1(rev .. file):sub(1, 20)
     local out_filename = hash .. "-" .. pandoc.path.filename(file)
@@ -23,19 +28,25 @@ function retrive_file(rev, file)
 
     pandoc.system.make_directory(out_dir, true)
     if not file_exists(out_filepath) then
-        local cmd = string.format("git show %s:%s >%s", rev, file, out_filepath)
+        local cmd = string.format("git -C %q show %s:%q >%q", repo, rev, file, out_filepath)
         os.execute(cmd)
     end
     return out_filename, out_dir
 end
 
-return {
-    ['git'] = function(args, kwargs, meta)
-        local rev = pandoc.utils.stringify(kwargs["rev"])
-        rev = (rev ~= "") and rev or "HEAD"
-        local file = pandoc.utils.stringify(args[1])
 
-        local out_filename, out_dir = retrive_file(rev, file)
-        return pandoc.path.join({ quarto.project.offset, "vcs", out_filename })
-    end
+local function git_include(args, kwargs, meta)
+    local repo_name = "."
+    local repo = get_repo(meta, repo_name)
+
+    local rev = pandoc.utils.stringify(kwargs["rev"])
+    rev = (rev ~= "") and rev or "HEAD"
+    local file = pandoc.utils.stringify(args[1])
+
+    local out_filename, out_dir = retrive_file(repo, rev, file)
+    return pandoc.path.join({ quarto.project.offset, "vcs", out_filename })
+end
+
+return {
+    ['git'] = git_include
 }
