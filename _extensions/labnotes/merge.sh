596de2850f819adb2dead34b2cceccd5161649c2
L-YQ <39945542+L-YQ@users.noreply.github.com>
2023-02-17 16:07:06 +0800

    ✨ build pdf by latexmk

--- _extensions/labnotes/merge.sh
+++ _extensions/labnotes/merge.sh
@@ -10,4 +10,5 @@ echo "$QUARTO_PROJECT_OUTPUT_FILES" | grep '.tex$' | xargs cat >> $out
 sed -n '/^%\$body\$$/,$p' "${script}/skeleton.tex" >> $out
 
 cd _build
-xelatex merged.tex 2>/dev/null
+latexmk -pdf -xelatex merged.tex
+latexmk -c merged.tex
